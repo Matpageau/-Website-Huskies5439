def69affdc52a88f68e1319d816b85d6c0385cd7
MatPageau <maladarix@gmail.com>
2025-04-08 17:53:34 -0400

    medias images edit

--- src/Utils/yearGallery.ts
+++ src/Utils/yearGallery.ts
@@ -1,20 +1,11 @@
-import fs from "fs";
-import path from "path";
+type PhotoGallery = {
+  [year: string]: string[];
+};
 
-export async function getYearPhotos(year: string) {
-  try {
+import photoGallery from "@/data/photoGallery.json" assert { type: "json" };
 
-    const dir = path.resolve(`./public/images/${year}/gallery`)
-    const fileNames = fs.readdirSync(dir)
-    const images = fileNames.map(name => `/images/${year}/gallery/${name}`)
-    
-    // const files = await fs.readdir(path.join(process.cwd(), `/public/images/${year}/gallery`));
-    // const filePaths = files.map(file => `/images/${year}/gallery/${file}`);
-    console.log(images);
-    
-    return images;
-  } catch (error) {
-    console.error("Une erreur est survenue en voulant chercher la gallerie photo", error);
-    return [];
-  }
+const typedGallery: PhotoGallery = photoGallery;
+
+export async function getYearPhotos(year: string): Promise<string[]> {
+  return typedGallery[year] || [];
 }
