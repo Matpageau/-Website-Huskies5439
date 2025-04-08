61cedb2669237894bee017f7eae872c023f7636e
MatPageau <maladarix@gmail.com>
2025-04-08 17:18:52 -0400

    Update yearGallery.ts

--- src/Utils/yearGallery.ts
+++ src/Utils/yearGallery.ts
@@ -1,13 +1,18 @@
-import fs from "fs/promises";
+import fs from "fs";
 import path from "path";
 
 export async function getYearPhotos(year: string) {
   try {
-    const files = await fs.readdir(path.join(process.cwd(), `/public/images/${year}/gallery`));
 
-    const filePaths = files.map(file => `/images/${year}/gallery/${file}`);
-
-    return filePaths;
+    const dir = path.resolve(`./public/images/${year}/gallery`)
+    const fileNames = fs.readdirSync(dir)
+    const images = fileNames.map(name => `/images/${year}/gallery/${name}`)
+    
+    // const files = await fs.readdir(path.join(process.cwd(), `/public/images/${year}/gallery`));
+    // const filePaths = files.map(file => `/images/${year}/gallery/${file}`);
+    console.log(images);
+    
+    return images;
   } catch (error) {
     console.error("Une erreur est survenue en voulant chercher la gallerie photo", error);
     return [];
